8d1e03a0ec72d18b796d5a6ff862721b66be8446
mrroll <58090032+mrroll@users.noreply.github.com>
2020-09-03 04:37:29 +0800

    - Fix overrides. - Separate configurations.

---
+++ common-overrides.js
@@ -0,0 +1,54 @@
+// Load this config last to ensure all rules overridden.
+
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ["plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  settings: {
+    // https://github.com/airbnb/javascript/issues/859#issuecomment-265862709
+    "import/resolver": {
+      node: {
+        moduleDirectory: [
+          "node_modules",
+          "src", // replace with your app-module-path directory
+        ],
+      },
+    },
+  },
+  rules: {
+    // Style
+    quotes: [1, "double"],
+
+    // https://github.com/airbnb/javascript/blob/64b965efe0355c8290996ff5a675cd8fb30bf843/packages/eslint-config-airbnb-base/rules/style.js#L334-L352
+    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+    "no-await-in-loop": "off",
+    "wrap-iife": "off",
+    "global-require": "off",
+    "func-names": "off",
+    "import/newline-after-import": "off",
+    "prefer-arrow-callback": "off",
+    "no-console": "warn",
+
+    // React
+    "react/jsx-filename-extension": "off",
+    "react/prop-types": "off",
+    "react/jsx-curly-newline": "off",
+    "react/jsx-fragments": "off",
+    "consistent-return": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "jsx-a11y/no-noninteractive-element-interactions": "off",
+
+    // NextJS
+    "react/react-in-jsx-scope": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "import/no-unresolved": ["error", { commonjs: true, ignore: [".+\\?.+$"] }],
+  },
+};
--- react.js
+++ react.js
@@ -4,35 +4,13 @@ module.exports = {
   parser: "babel-eslint",
   env: {
     browser: true,
-    es6: true,
   },
-  extends: ["airbnb", "plugin:prettier/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
+  extends: ["airbnb"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
     sourceType: "module",
   },
   plugins: ["react"],
-  settings: {
-    "react/jsx-filename-extension": "off",
-    "react/prop-types": "off",
-    "react/jsx-curly-newline": "off",
-    "react/jsx-fragments": "off",
-    "jsx-a11y/click-events-have-key-events": "off",
-    "jsx-a11y/no-noninteractive-element-interactions": "off",
-    // NextJS
-    "react/react-in-jsx-scope": "off",
-    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-    "import/no-unresolved": ["error", { commonjs: true, ignore: [".+\\?.+$"] }],
-    // Style
-    "no-console": "warn",
-    quotes: [1, "double"],
-  },
-  rules: {},
 };
