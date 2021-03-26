682712c810ba5ef64ed0acb62f00c911e9811c15
mrroll <58090032+mrroll@users.noreply.github.com>
2021-03-27 05:54:43 +0800

    Update dependencies.

--- react.js
+++ react.js
@@ -2,6 +2,8 @@ module.exports = {
   // For dynamic imports.
   // Requires babel-eslint.
   parser: "babel-eslint",
+  // Newer but requires config.
+  // parser: "@babel/eslint-parser",
   env: {
     browser: true,
   },
