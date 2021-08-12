b207529c3718ee5be8141f613e67a26954eaf89e
mrroll <58090032+mrroll@users.noreply.github.com>
2021-08-13 03:52:22 +0800

    3.5.0

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ module.exports = {
     "import/newline-after-import": 0,
     "no-console": 1,
     "no-unused-vars": 1,
+    "consistent-return": 0,
     "import/order": [
       1,
       {
@@ -96,30 +97,8 @@ module.exports = {
         ],
       },
     ],
-
-    /**
-     * React
-     */
-    "react/jsx-filename-extension": 0,
-    "react/prop-types": 0,
-    "react/jsx-curly-newline": 0,
-    "react/jsx-fragments": 0,
-    "consistent-return": 0,
-    "jsx-a11y/click-events-have-key-events": 0,
-    "jsx-a11y/no-noninteractive-element-interactions": 0,
-    "react/jsx-props-no-spreading": 1,
-
-    /**
-     * NextJS
-     */
-    "react/react-in-jsx-scope": 0,
     "import/no-extraneous-dependencies": [2, { devDependencies: true }],
     // next-optimized-images
     "import/no-unresolved": [2, { commonjs: true, ignore: [".+\\?.+$"] }],
-
-    /**
-     * Typescript
-     */
-    "@typescript-eslint/no-var-requires": 0,
   },
 };
--- react.js
+++ react.js
@@ -16,4 +16,14 @@ module.exports = {
     requireConfigFile: false,
   },
   plugins: ["react"],
+  rules: {
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "react/jsx-filename-extension": 0,
+    "react/prop-types": 0,
+    "react/jsx-curly-newline": 0,
+    "react/jsx-fragments": 0,
+    "react/jsx-props-no-spreading": 1,
+    "react/react-in-jsx-scope": 0,
+  },
 };
