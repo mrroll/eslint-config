e3ec53667ab85c405bd95fe657340c5d3063bbb6
mrroll <58090032+mrroll@users.noreply.github.com>
2022-05-13 17:22:31 +0800

    8.7.0

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ const config = {
     ...(isDependency("react") && {
       "jsx-a11y/click-events-have-key-events": 1,
       "jsx-a11y/no-noninteractive-element-interactions": 1,
-      "jsx-a11y/label-has-associated-control": "0",
+      "jsx-a11y/label-has-associated-control": 0,
       "react/jsx-props-no-spreading": 1,
       "react/jsx-filename-extension": 0,
       "react/prop-types": 0,
